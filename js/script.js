71c009dcca9d3d143686275a11792e487a593e53
Cherednikov Sergey <sergiys88ch@yandex.ru>
2021-08-08 19:16:55 +0300

    Первый пуш

---
+++ js/script.js
@@ -0,0 +1,30 @@
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-button');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+
+let popupOpen = () => {
+  popup.classList.add('popup__opened');
+}
+let popupClose = () => {
+  popup.classList.remove('popup__opened');
+}
+let formSubmitHandler = evt => {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popupClose();
+}
+let editProfile = () => {
+  popupOpen();
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', popupClose);
+editButton.addEventListener('click', editProfile);
