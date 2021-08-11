f2d8ab2f11f11dc0e71cbfd410b6709b8e553d97
Cherednikov Sergey <sergiys88ch@yandex.ru>
2021-08-12 00:56:29 +0300

    Третье исправление

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
@@ -8,10 +8,10 @@ let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
 let popupOpen = () => {
-  popup.classList.add('popup-opened');
+  popup.classList.add('popup_status_opened');
 }
 let popupClose = () => {
-  popup.classList.remove('popup-opened');
+  popup.classList.remove('popup_status_opened');
 }
 let formSubmitHandler = evt => {
     evt.preventDefault();
